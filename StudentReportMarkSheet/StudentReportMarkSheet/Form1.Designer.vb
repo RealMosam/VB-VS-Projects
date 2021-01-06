6f72cf081ea4ccf2312d83d2efb31155bc29180b
Mosam Shambharkar <36242921+RealMosam@users.noreply.github.com>
2021-01-06 22:18:49 +0530

    Add files via upload

---
+++ StudentReportMarkSheet/StudentReportMarkSheet/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ReportDataSource1 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Me.studentBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.report_dbDataSet = New StudentReportMarkSheet.report_dbDataSet()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Report1ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.studentTableAdapter = New StudentReportMarkSheet.report_dbDataSetTableAdapters.studentTableAdapter()
+        CType(Me.studentBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.report_dbDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'studentBindingSource
+        '
+        Me.studentBindingSource.DataMember = "student"
+        Me.studentBindingSource.DataSource = Me.report_dbDataSet
+        '
+        'report_dbDataSet
+        '
+        Me.report_dbDataSet.DataSetName = "report_dbDataSet"
+        Me.report_dbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'ReportViewer1
+        '
+        Me.ReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill
+        ReportDataSource1.Name = "DataSet1"
+        ReportDataSource1.Value = Me.studentBindingSource
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource1)
+        Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "StudentReportMarkSheet.Report1.rdlc"
+        Me.ReportViewer1.Location = New System.Drawing.Point(0, 28)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.ServerReport.BearerToken = Nothing
+        Me.ReportViewer1.Size = New System.Drawing.Size(1153, 422)
+        Me.ReportViewer1.TabIndex = 0
+        Me.ReportViewer1.Visible = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1153, 28)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Report1ToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(46, 24)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'Report1ToolStripMenuItem
+        '
+        Me.Report1ToolStripMenuItem.Name = "Report1ToolStripMenuItem"
+        Me.Report1ToolStripMenuItem.Size = New System.Drawing.Size(145, 26)
+        Me.Report1ToolStripMenuItem.Text = "Report1"
+        '
+        'studentTableAdapter
+        '
+        Me.studentTableAdapter.ClearBeforeFill = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1153, 450)
+        Me.Controls.Add(Me.ReportViewer1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Name = "Form1"
+        Me.Text = "Student Report by Mosam"
+        CType(Me.studentBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.report_dbDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents studentBindingSource As BindingSource
+    Friend WithEvents report_dbDataSet As report_dbDataSet
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Report1ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents studentTableAdapter As report_dbDataSetTableAdapters.studentTableAdapter
+End Class
---
+++ StudentReportMarkSheet/StudentReportMarkSheet/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'report_dbDataSet.student' table. You can move, or remove it, as needed.
+        Me.studentTableAdapter.Fill(Me.report_dbDataSet.student)
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+
+    Private Sub ReportViewer1_Load(sender As Object, e As EventArgs) Handles ReportViewer1.Load
+
+    End Sub
+
+    Private Sub Report1ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Report1ToolStripMenuItem.Click
+        ReportViewer1.Visible = True
+    End Sub
+End Class
